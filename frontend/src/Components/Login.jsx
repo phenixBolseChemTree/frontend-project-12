9e7a83a99235118e593830636bdb703a2beb8f79
Ivan <oldrum75@gmai.com>
2023-07-14 16:30:33 +0200

    feat: add Formik form

--- frontend/src/Components/Login.jsx
+++ frontend/src/Components/Login.jsx
@@ -1,10 +1,62 @@
+import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+
+const SignupSchema = Yup.object().shape({
+  firstName: Yup.string()
+    .min(2, 'Минимум 2 буквы')
+    .max(50, 'Максимум 50 букв')
+    .required('Обязательное поле'),
+  lastName: Yup.string()
+    .min(2, 'Минимум 2 буквы')
+    .max(50, 'Максимум 50 букв')
+    .required('Обязательное поле'),
+  email: Yup.string().email('Неверный email').required('Обязательное поле'),
+});
+
 const Login = () => {
-return (
-  <>
-  <h1>Здесь будет форма Formik1</h1>
-  </>
-)
-}
+  const initialValues = {
+    firstName: '',
+    lastName: '',
+    email: '',
+  };
 
+  const onSubmit = (values) => {
+    console.log(values);
+  };
 
-export default Login
+  return (
+    <>
+      <h1>Здесь будет форма Formik</h1>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={SignupSchema}
+        onSubmit={onSubmit}
+      >
+        <Form>
+          <div>
+            <label htmlFor="firstName">Имя</label>
+            <Field type="text" id="firstName" name="firstName" />
+            <ErrorMessage name="firstName" component="div" />
+          </div>
+
+          <div>
+            <label htmlFor="lastName">Фамилия</label>
+            <Field type="text" id="lastName" name="lastName" />
+            <ErrorMessage name="lastName" component="div" />
+          </div>
+
+          <div>
+            <label htmlFor="email">Email</label>
+            <Field type="email" id="email" name="email" />
+            <ErrorMessage name="email" component="div" />
+          </div>
+
+          <button type="submit">Отправить</button>
+        </Form>
+      </Formik>
+    </>
+  );
+};
+
+export default Login;
