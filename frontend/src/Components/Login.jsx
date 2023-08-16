3fc375f4eddf8951a57502e30f2cfa9fc4dc7b71
Ivan <oldrum75@gmai.com>
2023-08-16 20:57:38 +0200

    fix: add yup schema

--- frontend/src/Components/Login.jsx
+++ frontend/src/Components/Login.jsx
@@ -2,6 +2,17 @@ import React from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 
+const SignupSchema = Yup.object().shape({
+  firstName: Yup.string()
+    .min(2, 'Минимум 2 буквы')
+    .max(50, 'Максимум 50 букв')
+    .required('Обязательное поле'),
+  lastName: Yup.string()
+    .min(2, 'Минимум 2 буквы')
+    .max(50, 'Максимум 50 букв')
+    .required('Обязательное поле'),
+  email: Yup.string().email('Неверный email').required('Обязательное поле'),
+});
 
 const Login = () => {
   const initialValues = {
