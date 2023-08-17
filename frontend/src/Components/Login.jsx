ca467744d8bc085a345d4da8490455d57655c9fb
Ivan <oldrum75@gmai.com>
2023-08-17 20:19:43 +0200

    fix: delete invalid form

--- frontend/src/Components/Login.jsx
+++ frontend/src/Components/Login.jsx
@@ -25,38 +25,7 @@ const Login = () => {
     console.log(values);
   };
 
-  return (
-    <>
-      <h1>Здесь будет форма Formik</h1>
-      <Formik
-        initialValues={initialValues}
-        validationSchema={SignupSchema}
-        onSubmit={onSubmit}
-      >
-        <Form>
-          <div>
-            <label htmlFor="firstName">Имя</label>
-            <Field type="text" id="firstName" name="firstName" />
-            <ErrorMessage name="firstName" component="div" />
-          </div>
 
-          <div>
-            <label htmlFor="lastName">Фамилия</label>
-            <Field type="text" id="lastName" name="lastName" />
-            <ErrorMessage name="lastName" component="div" />
-          </div>
-
-          <div>
-            <label htmlFor="email">Email</label>
-            <Field type="email" id="email" name="email" />
-            <ErrorMessage name="email" component="div" />
-          </div>
-
-          <button type="submit">Отправить</button>
-        </Form>
-      </Formik>
-    </>
-  );
 };
 
 export default Login;
