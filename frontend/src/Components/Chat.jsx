3d388707ad5e11a50c7689e63c74dfce8b14c9e7
Ivan <oldrum75@gmai.com>
2023-08-29 21:00:56 +0200

    fix: Promise.resolve => useEffect

--- frontend/src/Components/Chat.jsx
+++ frontend/src/Components/Chat.jsx
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useNavigate } from 'react-router-dom';
 
 const Chat = () => {
   const navigate = useNavigate();
   const token = localStorage.token
-  if (token === undefined) {
-    Promise.resolve()
-      .then(() => navigate('/login'))
-  }
+    useEffect(() => {
+      if (token === undefined) {
+        // setTimeout(() => {
+          navigate('/login');
+        // }, 0);
+      }
+    }, []);
   return (
     <>
       <h1>Hello from chat</h1>
