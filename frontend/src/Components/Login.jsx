8762dff19fdf702b9e8443295920084f47bd3e68
Ivan <oldrum75@gmai.com>
2023-08-29 20:02:01 +0200

    feat: make redirect

--- frontend/src/Components/Login.jsx
+++ frontend/src/Components/Login.jsx
@@ -1,4 +1,5 @@
-import React, { useContext } from 'react';
+// import React, { useContext, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import axios from 'axios';
 import * as Yup from 'yup';
@@ -21,52 +22,53 @@ const Login = () => {
     lastName: '',
     email: '',
   };
+  const navigate = useNavigate();
+  // console.log(navigate('/'));
 
   const onSubmit = (values) => {
-    // console.log(values); http://localhost:5001/api/v1/data
     axios.post('/api/v1/login', { username: 'admin', password: 'admin' }).then((response) => {
-      const {token} = response
+      const { token } = response
       console.log(response.data); // => { token: ..., username: 'admin' }
       localStorage.setItem('token', token);
-    });
-    
-    // .then((response) => {
-      // localStorage.setItem(key, response);
-    // })
-  };
-
-  return (
-    <div className="container mt-5">
-      <h1>Здесь будет форма Formik</h1>
-      <Formik
-        initialValues={initialValues}
-        validationSchema={SignupSchema}
-        onSubmit={onSubmit}
-      >
-        <Form>
-          <div className="mb-3">
-            <label htmlFor="firstName" className="form-label">Имя</label>
-            <Field type="text" id="firstName" name="firstName" className="form-control" />
-            <ErrorMessage name="firstName" component="div" className="text-danger" />
-          </div>
-
-          <div className="mb-3">
-            <label htmlFor="lastName" className="form-label">Фамилия</label>
-            <Field type="text" id="lastName" name="lastName" className="form-control" />
-            <ErrorMessage name="lastName" component="div" className="text-danger" />
-          </div>
-
-          <div className="mb-3">
-            <label htmlFor="email" className="form-label">Email</label>
-            <Field type="email" id="email" name="email" className="form-control" />
-            <ErrorMessage name="email" component="div" className="text-danger" />
-          </div>
-
-          <button type="submit" className="btn btn-primary">Отправить</button>
-        </Form>
-      </Formik>
-    </div>
+      navigate('/')
+      // console.log(history);
+      // history.push('/');
+  }
   );
+};
+
+return (
+  <div className="container mt-5">
+    <h1>Здесь будет форма Formik</h1>
+    <Formik
+      initialValues={initialValues}
+      validationSchema={SignupSchema}
+      onSubmit={onSubmit}
+    >
+      <Form>
+        <div className="mb-3">
+          <label htmlFor="firstName" className="form-label">Имя</label>
+          <Field type="text" id="firstName" name="firstName" className="form-control" />
+          <ErrorMessage name="firstName" component="div" className="text-danger" />
+        </div>
+
+        <div className="mb-3">
+          <label htmlFor="lastName" className="form-label">Фамилия</label>
+          <Field type="text" id="lastName" name="lastName" className="form-control" />
+          <ErrorMessage name="lastName" component="div" className="text-danger" />
+        </div>
+
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">Email</label>
+          <Field type="email" id="email" name="email" className="form-control" />
+          <ErrorMessage name="email" component="div" className="text-danger" />
+        </div>
+
+        <button type="submit" className="btn btn-primary">Отправить</button>
+      </Form>
+    </Formik>
+  </div>
+);
 };
 
 export default Login;
