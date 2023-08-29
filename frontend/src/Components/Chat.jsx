1596d0700593088abf644ccc45ef259552585991
Ivan <oldrum75@gmai.com>
2023-08-29 21:12:28 +0200

    feat: get channels

--- frontend/src/Components/Chat.jsx
+++ frontend/src/Components/Chat.jsx
@@ -1,14 +1,23 @@
+import axios from "axios";
 import React, { useEffect } from "react";
 import { useNavigate } from 'react-router-dom';
 
 const Chat = () => {
   const navigate = useNavigate();
   const token = localStorage.token
-    useEffect(() => {
-      if (token === undefined) {
-          navigate('/login');
-      }
-    }, []);
+  useEffect(() => {
+    if (token === undefined) {
+      navigate('/login');
+    } else {
+      axios.get('/api/v1/data', {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }).then((response) => {
+        console.log(response.data); // => { channels: [...], currentChannelId: 1, messages: [] }
+      });
+    }
+  }, []);
   return (
     <>
       <h1>Hello from chat</h1>
