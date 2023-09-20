63ca213050825aba023980aabc142067a1dc7c50
Ivan <oldrum75@gmai.com>
2023-09-20 16:32:31 +0200

    fix: delete comments

--- frontend/src/Redux/store.js
+++ frontend/src/Redux/store.js
@@ -1,7 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-// import { combineReducers } from "@reduxjs/toolkit";
 import rootReducer from "./rootReducer";
-import chatSlice from "./chatSlice";
 
 
 const store = configureStore({
