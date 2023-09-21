c6f6b8423f8024337a3baf6c1b130956bb1ded95
Ivan <oldrum75@gmai.com>
2023-09-21 14:38:32 +0200

    fix: edit lockal

--- frontend/src/localization/i18n.js
+++ frontend/src/localization/i18n.js
@@ -12,12 +12,12 @@ i18n
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
     debug: true,
-    fallbackLng: 'en',
+    fallbackLng: 'ru',
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
     },
     resources: {
-      en: {
+      ru: {
         translation: {
           nav: {
             chatName: 'Hexlet Chat',
