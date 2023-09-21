5e6b08f885ccf51ea6aab22e2dbf949e469f38b0
Ivan <oldrum75@gmai.com>
2023-09-21 10:08:57 +0200

    feat: add all translated

--- frontend/src/localization/i18n.js
+++ frontend/src/localization/i18n.js
@@ -12,15 +12,55 @@ i18n
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
     debug: true,
-    fallbackLng: 'en',
+    fallbackLng: 'ru',
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
     },
     resources: {
-      en: {
-        translation: {
-          // here we will place our translations...
-        }
+      ru: {
+        nav: {
+          chatName: 'Hexlet Chat',
+          logOut: 'Выйти',
+        },
+        chat: {
+          channels: 'Каналы',
+          messages: 'сообщений',
+          form: 'Введите сообщение',
+        },
+        login: {
+          'come': 'Войти',
+          'yourName': 'Ваш ник',
+          'password': 'Пароль',
+          'no account': 'Нет аккаунта',
+          'registration': 'Регистрация'
+        },
+        signup: {
+          'registration': 'Регистрация',
+          'userName': 'Имя пользователя',
+          'password': 'Пароль',
+          'resPassword': 'Подтвердите пароль',
+          'btnRegistration': 'Зарегестрироваться'
+        },
+        errorPage: {
+          'pageIsNotFound': 'Страница не найдена',
+          'ButYouCanMoveOn': 'Но вы можете перейти',
+          'onMainPage': 'на главную страницу',
+          'resPassword': 'Подтвердите пароль',
+          'btnRegistration': 'Зарегестрироваться'
+        },
+        modal: {
+          addChannel: 'Добавить канал',
+          removeChannel: 'Удалить канал',
+          renameChannel: 'Переименовать канал',
+          btnCreate: 'Cоздать',
+          btnCancel: 'Отменить',
+          btnDelete: 'Удалить',
+          shure: 'Уверены?',
+        },
+        tubBar: {
+          channels: 'Удалить',
+          newChannel: 'Переименовать'
+        },
       }
     }
   });
