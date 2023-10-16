036350824158e760cce2fb6a5b4a39094b9b8174
Ivan <oldrum75@gmai.com>
2023-10-16 18:52:27 +0200

    feat: make new modal

--- frontend/src/сomponents/modal/MakeChannel.jsx
+++ frontend/src/сomponents/modal/MakeChannel.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
-import { Modal, Button } from 'react-bootstrap';
+import { Modal, Button, Form } from 'react-bootstrap';
+import { useFormik } from 'formik';
+import { useSocket } from '../SocketContext';
 
 const MakeChannel = ({ handleClose }) => {
-  console.log(123);
+  const socket = useSocket();
+  // console.log(123);
+
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+    },
+    onSubmit: (values) => {
+      const { name } = values;
+      console.log('name!!!!', name);
+      socket.emit('newChannel', { name });
+    },
+  });
 
   return (
     <Modal.Dialog>
@@ -10,14 +24,31 @@ const MakeChannel = ({ handleClose }) => {
         <Modal.Title>MakeChannel</Modal.Title>
       </Modal.Header>
       <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
-      <Modal.Footer>
-        <Button variant="secondary" onClick={handleClose}>
-          Close
-        </Button>
-        <Button variant="primary" onClick={handleClose}>
-          Save Changes
-        </Button>
-      </Modal.Footer>
+      <Form onSubmit={formik.handleSubmit}>
+        <Form.Group>
+          <Form.Control
+            required
+            type="text"
+            name="name"
+            placeholder="new Channel"
+            onChange={formik.handleChange}
+            value={formik.values.name}
+            isInvalid={!!formik.errors.name}
+          />
+          <Form.Label>random text</Form.Label>
+          <Form.Control.Feedback>
+            {formik.errors.name}
+          </Form.Control.Feedback>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+            <Button variant="primary" onClick={formik.handleSubmit}>
+              Save Changes
+            </Button>
+          </Modal.Footer>
+        </Form.Group>
+      </Form>
     </Modal.Dialog>
 
   );
--- frontend/src/сomponents/modal/RemoveChannel.jsx
+++ frontend/src/сomponents/modal/RemoveChannel.jsx
@@ -1,40 +1,14 @@
 import React from 'react';
 import { Modal, Button } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
-import { useSelector } from 'react-redux';
 import { useSocket } from '../SocketContext';
 
-// import { useFormik } from 'formik';
-// import yup from 'yup';
-
-const RemoveChannel = ({ handleClose }) => {
+const RemoveChannel = ({ handleClose, id }) => {
   const { t } = useTranslation();
   const socket = useSocket();
 
-  const currentChannelId = useSelector((state) => state.chat.currentChannelId);
-  // const channels = useSelector((state) => state.chat.channels);
-  // const channelNames = channels.map((channel) => channel.name);
-
-  // const validationSchema = yup.object().shape({
-  //   firstInput: yup.string()
-  //     .min(3, t('error.minWord3AndmaxWord20'))
-  //     .max(20, t('error.minWord3AndmaxWord20'))
-  //     .test('is-unique', t('modal.mustBeUnique'), (value) => !channelNames.includes(value))
-  //     .required(''),
-  // });
-
-  // const formik = useFormik({
-  //   initialValues: {
-
-  //   },
-  // });
-
-  const handleRemove = () => {
-    console.log('handleRemove!!!');
-    console.log('currentChannelId', currentChannelId);
-    socket.emit('removeChannel', { currentChannelId });
-    // notify('removeChannel');
-    // closeModal();
+  const handleSubmit = () => {
+    socket.emit('removeChannel', { id });
   };
 
   return (
@@ -47,7 +21,7 @@ const RemoveChannel = ({ handleClose }) => {
         <Button variant="secondary" onClick={handleClose}>
           Close
         </Button>
-        <Button variant="primary" onClick={handleRemove}>
+        <Button variant="primary" onClick={handleSubmit}>
           Save Changes
         </Button>
       </Modal.Footer>
