0e040e3887e7806d4b5fc7b1036ff250e1a00703
Ivan <oldrum75@gmai.com>
2023-10-16 03:24:12 +0200

    fix: rm modal index js

--- frontend/src/сomponents/modal/index.js
+++
@@ -1,9 +0,0 @@
-import RemoveChannel from './RemoveChannel';
-import RenameChannel from './RenameChannel';
-import MakeChannel from './MakeChannel';
-
-export default {
-  add: MakeChannel,
-  remove: RemoveChannel,
-  rename: RenameChannel,
-};
