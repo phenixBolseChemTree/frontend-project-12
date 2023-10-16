58792c612eadf9b6cf3b5c6b555e541fb453e304
Ivan <oldrum75@gmai.com>
2023-10-16 22:21:23 +0200

    fix: add locator

--- frontend/src/сomponents/modal/AddChannel.jsx
+++ frontend/src/сomponents/modal/AddChannel.jsx
@@ -1,15 +1,29 @@
 import React from 'react';
 import { Modal, Button, Form } from 'react-bootstrap';
 import { useFormik } from 'formik';
+import * as yup from 'yup';
+import { useTranslation } from 'react-i18next';
+import { useSelector } from 'react-redux';
 import { useSocket } from '../SocketContext';
 
 const AddChannel = ({ handleClose }) => {
   const socket = useSocket();
+  const channels = useSelector((state) => state.chat.channels);
+  const { t } = useTranslation();
+
+  const validationSchema = yup.object().shape({
+    name: yup.string()
+      .min(3, t('error.minWord3AndmaxWord20'))
+      .max(20, t('error.minWord3AndmaxWord20'))
+      .test('is-unique', t('modal.mustBeUnique'), (value) => !channels.map((channel) => channel.name).includes(value))
+      .required(''),
+  });
 
   const formik = useFormik({
     initialValues: {
       name: '',
     },
+    validationSchema,
     onSubmit: (values) => {
       const { name } = values;
       socket.emit('newChannel', { name });
@@ -29,13 +43,13 @@ const AddChannel = ({ handleClose }) => {
               required
               type="text"
               name="name"
-              placeholder="new Channel"
+              id="name"
               onChange={formik.handleChange}
               value={formik.values.name}
               isInvalid={!!formik.errors.name}
             />
             <Form.Label>random text</Form.Label>
-            <Form.Control.Feedback>
+            <Form.Control.Feedback type="invalid">
               {formik.errors.name}
             </Form.Control.Feedback>
             <Modal.Footer>
--- frontend/src/сomponents/modal/RenameChannel.jsx
+++ frontend/src/сomponents/modal/RenameChannel.jsx
@@ -1,16 +1,30 @@
 import React from 'react';
 import { Modal, Button, Form } from 'react-bootstrap';
 import { useFormik } from 'formik';
+import * as yup from 'yup';
+import { useTranslation } from 'react-i18next';
+import { useSelector } from 'react-redux';
 import { useSocket } from '../SocketContext';
 
 const RenameChannel = ({ handleClose, id }) => {
+  const { t } = useTranslation();
   console.log('handleClose!!!', handleClose);
   const socket = useSocket();
+  const channels = useSelector((state) => state.chat.channels);
+
+  const validationSchema = yup.object().shape({
+    name: yup.string()
+      .min(3, t('error.minWord3AndmaxWord20'))
+      .max(20, t('error.minWord3AndmaxWord20'))
+      .test('is-unique', t('modal.mustBeUnique'), (value) => !channels.map((channel) => channel.name).includes(value))
+      .required(''),
+  });
 
   const formik = useFormik({
     initialValues: {
       name: '',
     },
+    validationSchema,
     onSubmit: (values) => {
       const { name } = values;
       socket.emit('renameChannel', { id, name });
@@ -29,15 +43,14 @@ const RenameChannel = ({ handleClose, id }) => {
             <Form.Control
               required
               type="text"
-              placeholder="новое имя канала"
               name="name"
               id="name"
               onChange={formik.handleChange}
               value={formik.values.name}
               isInvalid={!!formik.errors.name}
             />
-            <Form.Label htmlFor="name">LOCATOR</Form.Label>
-            <Form.Control.Feedback>
+            <Form.Label htmlFor="name">Переименовать</Form.Label>
+            <Form.Control.Feedback type="invalid">
               {formik.errors.name}
             </Form.Control.Feedback>
             <Modal.Footer>
