126655e12a42d404820c30d3bdc0199bc0df366f
Ivan Sharapov <oldrum75@gmail.com>
2023-10-29 02:07:50 +0100

    fix: socket connection occurs internally init

--- frontend/src/init.jsx
+++ frontend/src/init.jsx
@@ -3,13 +3,14 @@ import i18next from 'i18next';
 import { I18nextProvider, initReactI18next } from 'react-i18next';
 import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
+import io from 'socket.io-client'; // Импортируйте io здесь
 import resources from './locales/index';
 import App from './components/App';
-import SocketConnect from './components/SocketConnect';
+import { AuthProvider } from './components/AuthContext';
 import { SocketProvider } from './components/SocketProvider';
+
 import 'react-toastify/dist/ReactToastify.css';
 import reducer from './slice';
-import { AuthProvider } from './components/AuthContext';
 
 const init = async () => {
   const i18n = i18next.createInstance();
@@ -27,12 +28,14 @@ const init = async () => {
     reducer,
   });
 
+  // Инициализируйте сокс здесь
+  const socket = io();
+
   return (
     <Provider store={store}>
       <I18nextProvider i18n={i18n}>
         <AuthProvider>
-          <SocketProvider>
-            <SocketConnect />
+          <SocketProvider socket={socket}>
             <App />
           </SocketProvider>
         </AuthProvider>
