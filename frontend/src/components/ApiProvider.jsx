cbc059a4f7fdb1ae4a72fb95612d8715fe7c397c
Ivan Sharapov <oldrum75@gmail.com>
2023-10-30 22:50:39 +0100

    fix: rm unusable file

--- frontend/src/components/ApiProvider.jsx
+++ frontend/src/components/ApiProvider.jsx
@@ -4,23 +4,4 @@ const SocketContext = createContext();
 
 const useApi = () => useContext(SocketContext);
 
-// const actions = {
-//   newMessage: (data) => {
-//     socket.emit('newMessage', {
-//       body: data.validatedText,
-//       username: data.localStorage.username,
-//       channelId: data.currentChannelId,
-//     });
-//   },
-//   newChannel: (data) => {
-//     socket.emit('newChannel', { name: data.name });
-//   },
-//   removeChannel: (data) => {
-//     socket.emit('removeChannel', { id: data.id });
-//   },
-//   renameChannel: (data) => {
-//     socket.emit('renameChannel', { id: data.id, name: data.name });
-//   },
-// };
-
 export { useApi, SocketContext };
