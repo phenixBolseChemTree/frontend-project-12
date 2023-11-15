6f994ca6c75be79dfc4255741372c73bb6e3c7e9
Ivan Sharapov <oldrum75@gmail.com>
2023-11-15 11:55:20 +0100

    fix: rm log

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -11,24 +11,21 @@ import {
 } from './pages/pages';
 import ModalWindow from './modal/ModalWindow';
 
-const App = () => {
-  console.log(123);
-  return (
-    <AuthProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<NavPage />}>
-            <Route path="/" element={<Chat />} />
-            <Route path="signup" element={<Signup />} />
-            <Route path="login" element={<Login />} />
-            <Route path="*" element={<Page404 />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-      <ToastContainer />
-      <ModalWindow />
-    </AuthProvider>
-  );
-};
+const App = () => (
+  <AuthProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<NavPage />}>
+          <Route path="/" element={<Chat />} />
+          <Route path="signup" element={<Signup />} />
+          <Route path="login" element={<Login />} />
+          <Route path="*" element={<Page404 />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+    <ToastContainer />
+    <ModalWindow />
+  </AuthProvider>
+);
 
 export default App;
