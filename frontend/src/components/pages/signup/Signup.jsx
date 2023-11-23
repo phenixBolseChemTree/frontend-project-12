e10bc8254d4c10ff160b3f8f54c331672200b850
Ivan Sharapov <oldrum75@gmail.com>
2023-11-23 12:30:59 +0100

    comment test

--- frontend/src/components/pages/signup/Signup.jsx
+++ frontend/src/components/pages/signup/Signup.jsx
@@ -3,7 +3,7 @@ import {
 } from 'react-bootstrap';
 
 import { useTranslation } from 'react-i18next';
-
+//
 import img from '../../../assets/happy_man.jpg';
 import SignupForm from './SignupForm';
 
