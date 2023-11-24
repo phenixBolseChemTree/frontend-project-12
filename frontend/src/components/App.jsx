a7a8d9ccacb4e659d324407a5b87fe2c4c527bdb
Ivan Sharapov <oldrum75@gmail.com>
2023-11-24 11:48:27 +0100

    fix: rm other file pages

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -7,9 +7,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '../App.css';
 import { ToastContainer } from 'react-toastify';
 import { AuthProvider } from './AuthContext';
-import {
-  NavPage, Login, Signup, Page404, RenderChat,
-} from './pages/pages';
+// import {
+//   NavPage, Login, Signup, Page404, RenderChat,
+// } from './pages/pages';
+import NavPage from './pages/navPage/NavPage';
+import RenderChat from './pages/chat/RenderChat';
+import Signup from './pages/signup/Signup';
+import Login from './pages/login/Login';
+import Page404 from './pages/page404/Page404';
+
 import ModalWindow from './modal/ModalWindow';
 
 const App = () => (
