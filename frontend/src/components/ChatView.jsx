0f2bcdd3d94d3938091159f676452a19b96edd4d
Ivan Sharapov <oldrum75@gmail.com>
2023-12-11 12:00:27 +0100

    feat: add scroll if you add new message

--- frontend/src/components/ChatView.jsx
+++ frontend/src/components/ChatView.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useSelector } from 'react-redux';
 import ChatForm from './ChatForm';
@@ -11,43 +11,56 @@ const currentNameChannel = (channels, id) => {
   return null;
 };
 
-const getCurrentMessages = (messages, selectedChannel) => messages.filter(
-  ({ channelId }) => channelId === selectedChannel,
+const getCurrentMessages = (messages, selectedChannel) => (
+  messages.filter(({ channelId }) => channelId === selectedChannel)
 );
 
 const ChatView = () => {
   const { messages, channels, currentChannelId } = useSelector((state) => state.chat);
 
   const { t } = useTranslation();
-  const nameChanel = currentNameChannel(channels, currentChannelId);
+  const nameChannel = currentNameChannel(channels, currentChannelId);
   const currentMessages = getCurrentMessages(messages, currentChannelId);
 
-  const messageText = t('chat.messages.key', { count: currentMessages.length });
+  const messagesEndRef = useRef(null);
+
+  const scrollToBottom = () => {
+    messagesEndRef.current?.scrollIntoView({ behavior: 'auto', block: 'end' });
+  };
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [messages]);
 
   return (
     <div className="col p-0 h-100 chat-board">
       <div className="chat d-flex flex-column h-100">
-        <div className="bg-light mb-4 p-3 shadow-sm small ">
+        <div className="bg-light mb-4 p-3 shadow-sm small">
           <p className="m-0">
             <b>
               #
               {' '}
-              {nameChanel}
+              {nameChannel}
             </b>
           </p>
-          <span className="text-muted">{messageText}</span>
+          <span className="text-muted">
+            {t('chat.messages.key', { count: currentMessages.length })}
+          </span>
         </div>
         <div id="messages-box" className="chat-messages overflow-auto px-5">
-          {messages.length !== 0
-            && currentMessages.map(({ body, username, id }) => (
-              <div className="text-break mb-2" key={id}>
-                <b>{username}</b>
-                :
-                {' '}
-                {body}
-              </div>
-
-            ))}
+          {currentMessages.map(({ body, username, id }, index) => (
+            <div
+              className="text-break mb-2"
+              key={id}
+              ref={index === currentMessages.length - 1 ? messagesEndRef : null}
+            >
+              <b>{username}</b>
+              :
+              {' '}
+              {body}
+            </div>
+          ))}
+          <div ref={messagesEndRef} />
         </div>
         <div className="mt-auto px-5 py-3 message-box">
           <ChatForm />
