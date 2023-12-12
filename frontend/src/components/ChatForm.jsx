e8c7d00ac6f9e2c3780b466f00016893f94ec59c
Ivan Sharapov <oldrum75@gmail.com>
2023-12-12 16:04:35 +0100

    fix: bootrap style

--- frontend/src/components/ChatForm.jsx
+++ frontend/src/components/ChatForm.jsx
@@ -3,7 +3,7 @@ import { useTranslation } from 'react-i18next';
 import { useSelector } from 'react-redux';
 import { useFormik } from 'formik';
 import * as yup from 'yup';
-import { Button, Form } from 'react-bootstrap';
+import { ButtonGroup, Form } from 'react-bootstrap';
 import leoFilter from 'leo-profanity';
 import { useApi } from '../context/ApiContext';
 import { useAuth } from '../context/AuthContext';
@@ -53,17 +53,18 @@ const ChatForm = () => {
           onChange={formik.handleChange}
           autoFocus
         />
-        <Button
+        <ButtonGroup
+          vertical
           variant="light"
           type="submit"
-          className="btn btn-group-vertical"
+          className="btn"
           disabled={formik.isSubmitting}
         >
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="20" height="20" fill="currentColor">
             <path fillRule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm4.5 5.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z" />
           </svg>
           <span className="visually-hidden">{t('chat.send')}</span>
-        </Button>
+        </ButtonGroup>
       </Form.Group>
     </Form>
   );
