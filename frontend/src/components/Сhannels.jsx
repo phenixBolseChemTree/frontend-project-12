1fd13cdf7835f71763f98aadd17cc4ebede46e96
Ivan Sharapov <oldrum75@gmail.com>
2023-12-14 11:58:43 +0100

    fix: add new btn

--- frontend/src/components/Сhannels.jsx
+++ frontend/src/components/Сhannels.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { useDispatch } from 'react-redux';
-import { ButtonGroup } from 'react-bootstrap';
+import { Button, ButtonGroup } from 'react-bootstrap';
 import ButtonChannel from './ChannelButton';
 import { openModal } from '../slice';
 
@@ -21,7 +21,7 @@ const Chanells = ({ channels }) => {
             <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
             <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
           </svg>
-          <span className="visually-hidden">+</span>
+          <Button className="visually-hidden">+</Button>
         </ButtonGroup>
       </div>
       <ul className="nav flex-column nav-pills nav-fill px-2 mb-3 overflow-auto h-100 d-block">
