7b26564b196251bb5c108614e6b189dabd46e596
Ivan Sharapov <oldrum75@gmail.com>
2023-12-14 11:50:16 +0100

    fix: add Button group

--- frontend/src/components/Сhannels.jsx
+++ frontend/src/components/Сhannels.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { useDispatch } from 'react-redux';
-import { Button } from 'react-bootstrap';
+import { ButtonGroup } from 'react-bootstrap';
 import ButtonChannel from './ChannelButton';
 import { openModal } from '../slice';
 
@@ -16,13 +16,13 @@ const Chanells = ({ channels }) => {
     <div className="col-4 col-md-2 border-end px-0 bg-light flex-column h-100 d-flex">
       <div className="d-flex mt-1 justify-content-between mb-2 ps-4 pe-2 p-4">
         <b>{t('chat.channels')}</b>
-        <Button onClick={() => dispatch(openModal({ type: 'addChannel' }))} variant="light" type="button" className="p-0 text-primary btn btn-group-vertical">
+        <ButtonGroup vertical onClick={() => dispatch(openModal({ type: 'addChannel' }))} variant="light" type="button" className="p-0 text-primary btn">
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="20" height="20" fill="currentColor">
             <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
             <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
           </svg>
           <span className="visually-hidden">+</span>
-        </Button>
+        </ButtonGroup>
       </div>
       <ul className="nav flex-column nav-pills nav-fill px-2 mb-3 overflow-auto h-100 d-block">
         {channels.length !== 0 && channels.map(({ id, name, removable }) => (
